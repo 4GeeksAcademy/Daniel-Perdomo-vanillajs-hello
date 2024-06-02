6cafa0274f85210e9dc07fd619aeb4c8472e82bd
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-06-02 21:00:12 +0000

    random generator + phrase using it

--- src/app.js
+++ src/app.js
@@ -8,17 +8,32 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  let excuse = "My cat destroyed my parrot";
-  document.getElementById("excuse").innerHTML = excuse;
-};
 
-let who = ["The dog", "My grandma", "The mailman", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "my phone", "the car"];
-let when = [
-  "before the class",
-  "when I was sleeping",
-  "while I was exercising",
-  "during my lunch",
-  "while I was praying",
-];
+  function randomNumber(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+
+  document.getElementById("excuse").innerHTML = excuse;
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird", "My wife"];
+  let whoPart = who[randomNumber(who.length - 1)];
+
+  let action = ["ate", "peed", "crushed", "broke"];
+  let actionPart = action[randomNumber(action.length - 1)];
+
+  let what = ["my homework", "my phone", "the car"];
+  let whatPart = what[randomNumber(what.length - 1)];
+
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+    "while playing the trombone",
+  ];
+  let whenPart = when[randomNumber(when.length - 1)];
+
+  let phrase = `${whoPart} ${actionPart} ${whatPart} ${whenPart}`;
+  document.getElementById("excuse").innerHTML = phrase;
+};
